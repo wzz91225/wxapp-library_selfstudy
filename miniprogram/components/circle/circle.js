70892ca4b990e393eea954196ff7b3bf8810024d
yugugugu <2399304421@qq.com>
2020-05-28 22:38:10 +0800

    ly - alter UI

---
+++ miniprogram/components/circle/circle.js
@@ -0,0 +1,31 @@
+// components/circle/circle.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    obvious:{
+      type: String,
+      value:"-----"//默认值
+    },
+    degree:{
+      type: String,
+      value:"c"//默认绿色
+    }
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
---
+++ miniprogram/pages/orderview/orderview.js
@@ -0,0 +1,93 @@
+// pages/orderview/orderview.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    aa:[
+      {
+        nickName:"wang",
+        reward:"failed"
+      },
+      {
+        nickName: "wang",
+        reward: "failed"
+      },
+      {
+        nickName: "wang",
+        reward: "failed"
+      },
+      {
+        nickName: "wang",
+        reward: "success"
+      },
+      {
+        nickName: "wang",
+        reward: "success"
+      },
+     
+    ],
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  //---------------------------
+  //---------------------------
+  //---------------------------
+  //完成拉取预约信息，从数据库中
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
