c2ba8ca96eb231161eda0d2af485bc08aaa40b3c
Apllasssa <1044899079@qq.com>
2020-05-29 12:04:07 +0800

    djh - add

---
+++ miniprogram/pages/updateinfor/updateInfor.js
@@ -0,0 +1,65 @@
+const app = getApp()
+// function upd(x,database){//x is new value of status
+//     const db = wx.cloud.database()
+//     db.collection(database).where({
+//         _openid: app.globalData.openid
+//     }).update({
+//       data: {
+//         status : x
+//       },
+//       success: res => {
+//         console.log("success!")
+//       },
+//       fail: err => {
+//         icon: 'none',
+//         console.error('[数据库] [更新记录] 失败：', err)
+//       }
+//     })
+// }
+function upd(x) {
+  wx.cloud.callFunction({
+    name: 'update',//modify user
+    data: {
+      //doneTime: util.formatTime(new Date())
+      status:x
+    },
+    success: res => {
+      console.log("update success")
+      wx.showToast({
+        title: 'success!',
+        success: function () {
+          
+      }
+    })
+    },
+    fail: err => {
+      console.error
+    }
+  });
+}
+function upseat(x,y) {//x:status y:seat_num
+  wx.cloud.callFunction({
+    name: 'update_seat',//modify user
+    data: {
+      //doneTime: util.formatTime(new Date())
+      status:x,
+      seat_num:y
+    },
+    success: res => {
+      console.log("update success")
+      wx.showToast({
+        title: 'success!',
+        success: function () {
+          
+      }
+    })
+    },
+    fail: err => {
+      console.error
+    }
+  });
+}
+module.exports = {
+    upd: upd,
+    upseat:upseat
+  }
