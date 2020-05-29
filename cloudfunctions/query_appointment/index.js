784550211b6c3aa3daf98f1f4f325274d043898c
Apllasssa <1044899079@qq.com>
2020-05-29 09:18:25 +0800

    djh - add cloudfunction(query appointment details)

---
+++ cloudfunctions/query_appointment/index.js
@@ -0,0 +1,37 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+cloud.init()
+const db = cloud.database()
+exports.main = async (event, context) => {
+  const wxContext = cloud.getWXContext()
+  try {
+    return await db.collection('appointment').where({
+    //_openid: wxContext.OPENID,
+      seat_num:event.seat_num
+    }).get({
+      success: function (res) {
+        return res
+      }
+    });
+  }
+  catch (e) {
+    console.error(e);
+  }
+  // db.collection('appointment').where({
+  //   //_openid: wxContext.OPENID
+  //     time:"2020-5-12"
+  //   }).orderBy('time','asc').get({
+  //     success:function(res){
+  //       return res.data
+  //       console.log("success")
+  //     } 
+  //   })
+
+  // try{
+  //   return await db.collection('appointment').orderBy('time','desc').orderBy('time','asc').get({
+  //     success:function(res){
+  //       console.log("这是升序排序:"+res)
+  //     }
+  //   });
+  
+}
