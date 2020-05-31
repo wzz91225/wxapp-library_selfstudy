b381dcbf3826b8e315ee246a00d2eee414edb233
Apllasssa <1044899079@qq.com>
2020-05-31 21:24:29 +0800

    djh - update seat

---
+++ cloudfunctions/updateSeat/index.js
@@ -0,0 +1,27 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init()
+
+
+const db = cloud.database()
+exports.main = async (event, context) => {
+  let tmp = await db.collection('seat').where({
+    seatNum: event.seat // 填入当前用户 openid
+  }).get()
+  //return tmp
+  if(tmp.data[0].status==1){
+    await db.collection('seat').where({
+      seatNum:event.seat
+    }).update({
+      data:{
+        status:2
+      }
+    })
+    return 1
+  }
+  else{
+    return 0
+  }
+ 
+}
