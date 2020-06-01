ee9bb36138c28536599e5d47b43098a6fbef4d61
shijiexuexi <zhao275064940@qq.com>
2020-06-02 02:32:00 +0800

    zyh -trigger-22_end

---
+++ cloudfunctions/trigger_end/index.js
@@ -0,0 +1,57 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+cloud.init()
+const db = cloud.database()
+const _ = db.command
+
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  const wxContext = cloud.getWXContext()
+  var aa=await db.collection('seat').get({
+    success:function(res) { //成功打印消息
+      console.log('3', res)
+    },
+     fail(res) { //存入数据库失败
+       console.log('订单存入数据库操作失败');
+      //云函数更新
+     }
+    })
+    for(var i=0;i<aa.data.length;i++){
+      if(aa.data[i].status==4){
+
+      }
+    }
+    await db.collection('seat').where({
+      status:3
+    }).update({
+      data:{
+        status:1
+      },
+      success:function(res) { //成功打印消息
+        console.log('3', res)
+      },
+       fail(res) { //存入数据库失败
+         console.log('订单存入数据库操作失败');
+        //云函数更新
+       }
+      })
+      await db.collection('user').update({
+        data:{
+          status:3
+        },
+        success:function(res) { //成功打印消息
+          console.log('3', res)
+        },
+         fail(res) { //存入数据库失败
+           console.log('订单存入数据库操作失败');
+          //云函数更新
+         }
+        })
+  return {
+    event,
+    openid: wxContext.OPENID,
+    appid: wxContext.APPID,
+    unionid: wxContext.UNIONID,
+  }
+}
