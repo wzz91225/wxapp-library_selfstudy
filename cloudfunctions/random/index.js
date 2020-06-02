437a570faabab4c5cf17b35e1537aacdfd3063fa
shijiexuexi <zhao275064940@qq.com>
2020-06-02 10:08:48 +0800

    zyh -random -end

--- cloudfunctions/random/index.js
+++ cloudfunctions/random/index.js
@@ -4,8 +4,10 @@ const db = cloud.database()
 // 云函数入口函数
 exports.main = async (event, context) => {
   //const wxContext = cloud.getWXContext()
+  var num=-1
   let order = await db.collection('appointment').field({
-    _openid:true
+    _openid:true,
+    result:'3'
   }).get()
   var i
   var list=[]
@@ -16,8 +18,8 @@ exports.main = async (event, context) => {
     list[i]=cre.data[0].credit
   }
   var len
-  if(order.data.length>=3){
-    len=3
+  if(order.data.length>=4){
+    len=4
   }else{
     len=order.data.length
   }
@@ -58,17 +60,268 @@ exports.main = async (event, context) => {
       list1[i]=k
     }
   }
-  //return list1
+//  return list1
 
-  for(i=0;i<len;i++){
-    await db.collection('seat').where({
-      seatNum:list1[i]
-    }).update({
-      data:{
-        status:3,
-        openid:order.data[i]._openid
+  for(var i=0;i<order.data.length;i++){
+    var aa_1=await db.collection('appointment').where({
+      _openid:order.data[i]._openid,
+      timeFlag:'1',
+      result:3
+    }).get({
+        success:function(res) { //成功打印消息
+          console.log('3', res)
+        },
+         fail(res) { //存入数据库失败
+           console.log('订单存入数据库操作失败');
+          //云函数更新
+         }
+        })
+    if(aa_1.data.length!=0){
+      num=num+1
+      if(num>3){
+        db.collection('appointment').where({
+          result:3
+         }).update({
+          data:{
+            result:2
+          },
+         success(res) { //成功打印消息
+           console.log('3', res)
+         },
+         fail(res) { //存入数据库失败
+           console.log('订单存入数据库操作失败');
+          //云函数更新
+         }
+        })
+        return
       }
-    })
+    }
+    db.collection('appointment').where({
+     _openid:order.data[i]._openid,
+     timeFlag:'1',
+     result:3
+    }).update({
+     data:{
+      seatNum:list1[num],
+      result:1
+    }, 
+    success(res) { //成功打印消息
+      console.log('3', res)
+    },
+    fail(res) { //存入数据库失败
+      console.log('订单存入数据库操作失败');
+     //云函数更新
+    }
+   })
   }
+  for(var i=0;i<order.data.length;i++){
+    var aa_2=await db.collection('appointment').where({
+      _openid:order.data[i]._openid,
+      timeFlag:'2',
+      result:3
+    }).get({
+        success:function(res) { //成功打印消息
+          console.log('3', res)
+        },
+         fail(res) { //存入数据库失败
+           console.log('订单存入数据库操作失败');
+          //云函数更新
+         }
+        })
+    if(aa_2.data.length!=0){
+      num=num+1
+      if(num>3){
+        db.collection('appointment').where({
+          result:3
+         }).update({
+          data:{
+            result:2
+          },
+         success(res) { //成功打印消息
+           console.log('3', res)
+         },
+         fail(res) { //存入数据库失败
+           console.log('订单存入数据库操作失败');
+          //云函数更新
+         }
+        })
+        return
+      }
+    }
+    db.collection('appointment').where({
+     _openid:order.data[i]._openid,
+     timeFlag:'2',
+     result:3
+    }).update({
+     data:{
+      seatNum:list1[num],
+      result:1
+    }, 
+    success(res) { //成功打印消息
+      console.log('3', res)
+    },
+    fail(res) { //存入数据库失败
+      console.log('订单存入数据库操作失败');
+     //云函数更新
+    }
+   })
+  }
+  for(var i=0;i<order.data.length;i++){
+    var aa_3=await db.collection('appointment').where({
+      _openid:order.data[i]._openid,
+      timeFlag:'3',
+      result:3
+    }).get({
+        success:function(res) { //成功打印消息
+          console.log('3', res)
+        },
+         fail(res) { //存入数据库失败
+           console.log('订单存入数据库操作失败');
+          //云函数更新
+         }
+        })
+    if(aa_3.data.length!=0){
+      num=num+1
+      if(num>3){
+        db.collection('appointment').where({
+          result:3
+         }).update({
+          data:{
+            result:2
+          },
+         success(res) { //成功打印消息
+           console.log('3', res)
+         },
+         fail(res) { //存入数据库失败
+           console.log('订单存入数据库操作失败');
+          //云函数更新
+         }
+        })
+        return
+      }
+    }
+    db.collection('appointment').where({
+     _openid:order.data[i]._openid,
+     timeFlag:'3',
+     result:3
+    }).update({
+     data:{
+      seatNum:list1[num],
+      result:1
+    }, 
+    success(res) { //成功打印消息
+      console.log('3', res)
+    },
+    fail(res) { //存入数据库失败
+      console.log('订单存入数据库操作失败');
+     //云函数更新
+    }
+   })
+  }
+  for(var i=0;i<order.data.length;i++){
+    var aa_4=await db.collection('appointment').where({
+      _openid:order.data[i]._openid,
+      timeFlag:'4',
+      result:3
+    }).get({
+        success:function(res) { //成功打印消息
+          console.log('3', res)
+        },
+         fail(res) { //存入数据库失败
+           console.log('订单存入数据库操作失败');
+          //云函数更新
+         }
+        })
+    if(aa_4.data.length!=0){
+      num=num+1
+      if(num>3){
+         db.collection('appointment').where({
+          result:3
+         }).update({
+          data:{
+            result:2
+          },
+         success(res) { //成功打印消息
+           console.log('3', res)
+         },
+         fail(res) { //存入数据库失败
+           console.log('订单存入数据库操作失败');
+          //云函数更新
+         }
+        })
+        return
+      }
+    }
+    db.collection('appointment').where({
+     _openid:order.data[i]._openid,
+     timeFlag:'4',
+     result:3
+    }).update({
+     data:{
+      seatNum:list1[num],
+      result:1
+    }, 
+    success(res) { //成功打印消息
+      console.log('3', res)
+    },
+    fail(res) { //存入数据库失败
+      console.log('订单存入数据库操作失败');
+     //云函数更新
+    }
+   })
+  }
+  for(var i=0;i<order.data.length;i++){
+    var aa_5=await db.collection('appointment').where({
+      _openid:order.data[i]._openid,
+      timeFlag:'5',
+      result:3
+    }).get({
+        success:function(res) { //成功打印消息
+          console.log('3', res)
+        },
+         fail(res) { //存入数据库失败
+           console.log('订单存入数据库操作失败');
+          //云函数更新
+         }
+        })
+    if(aa_5.data.length!=0){
+      num=num+1
+      if(num>3){
+        db.collection('appointment').where({
+          result:3
+         }).update({
+          data:{
+            result:2
+          },
+         success(res) { //成功打印消息
+           console.log('3', res)
+         },
+         fail(res) { //存入数据库失败
+           console.log('订单存入数据库操作失败');
+          //云函数更新
+         }
+        })
+        return
+      }
+    }
+    db.collection('appointment').where({
+     _openid:order.data[i]._openid,
+     timeFlag:'5',
+     result:3
+    }).update({
+     data:{
+      seatNum:list1[num],
+      result:1
+    }, 
+    success(res) { //成功打印消息
+      console.log('3', res)
+    },
+    fail(res) { //存入数据库失败
+      console.log('订单存入数据库操作失败');
+     //云函数更新
+    }
+   })
+  }
+  
   return db.collection('seat').get()
 }
