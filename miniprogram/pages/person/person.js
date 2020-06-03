a3385de47decc92901eb740ca1adb7ffa83d5e67
wzz91225 <wzz91225@outlook.com>
2020-06-04 00:22:51 +0800

    wzz - add refresh; fix bug from zyh

--- miniprogram/pages/person/person.js
+++ miniprogram/pages/person/person.js
@@ -6,32 +6,16 @@ Page({
    * 页面的初始数据
    */
   data: {
-    credit:"0",    //这个表示信用分显示
+    credit:"0",    //信用分
     openid: ""
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    this.setData({
-      credit:app.data.Credit,
-      openid:app.globalData.openid
-    })
-  },
+  
+  updateDataFromCloud: function(){
+    var _this = this
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+    // console.log(app.globalData.openid)
 
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    console.log(app.globalData.openid)
     const db = wx.cloud.database()
     db.collection('user').where({
       _openid: app.globalData.openid
@@ -43,12 +27,54 @@ Page({
         // this.setData({
         //   currentStatus : (app.data.userStatus - 1)
         // })
-        this.changCurrentStatus(app.data.userStatus - 1)
+        // this.changCurrentStatus(app.data.userStatus - 1)
 
-        console.log("app data:"+app.data)
-        console.log("app globaldata:"+app.globalData)
+        console.log("[云函数]查询用户信息成功", res)
+        // console.log("app data:"+app.data)
+        // console.log("app globaldata:"+app.globalData)
+    
+        _this.setData({
+          credit: app.data.Credit,
+          openid: app.globalData.openid
+        })
+      },
+
+      fail: err => {
+        console.log("[云函数]查询用户信息失败", err)
+      },
+
+      complete: function(){
+        wx.stopPullDownRefresh({
+          complete(res) {
+            wx.hideToast()
+            console.log(res, new Date())
+          }
+        })
       }
     })
+  },
+
+
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.updateDataFromCloud()
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    this.updateDataFromCloud()
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    this.updateDataFromCloud()
   },
 
   /**
@@ -69,7 +95,7 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    this.updateDataFromCloud()
   },
 
   /**
