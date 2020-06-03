18e9169125bcdc490d49095abaa4a249d60cf0de
wzz91225 <wzz91225@outlook.com>
2020-06-04 01:55:47 +0800

    wzz - add refresh; alter ui; fix bug

--- miniprogram/pages/accuse/accuse.js
+++ miniprogram/pages/accuse/accuse.js
@@ -10,27 +10,8 @@ Page({
     accuseSelectNum:[],//这里放入传入的桌号
     tableNum:[],//这里放入传入的桌号
     rangekey: 0,
-    accuseText:'null',//
-    accusecause:[
-      {
-        id:1,
-        cause:"王"
-      },
-      {
-        id:2,
-        cause:"者"
-      },
-      {
-        id:3,
-        cause:"荣"
-      },
-      {
-        id:4,
-        cause:"耀"
-      },
-      
+    accuseText: "",
 
-    ],
     seatNum:null,
     reason:'',
     motto: 'Hello World',
@@ -55,14 +36,45 @@ Page({
     this.setData({
       accuseSelectNumIndex:e.detail.value
     })
-},
-textChange:function(e){
-    console.log('accuseText举报文字的值为',e.detail.value)
-    this.setData({
-      accuseText:e.detail.value
-    })
+  },
+  textChange:function(e){
+      console.log('accuseText举报文字的值为',e.detail.value)
+      this.setData({
+        accuseText:e.detail.value
+      })
+  },
 
-},
+
+  clearContent: function(){
+    this.setData({
+      accuseSelectNumIndex: 0,
+      accuseText: "",
+      imageSrc: "",
+      hasUploadImage: false
+    })
+  },
+
+
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    this.clearContent()
+
+    wx.stopPullDownRefresh({
+      complete(res) {
+        wx.hideToast()
+        console.log(res, new Date())
+      }
+    })
+  },
+
+
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
   onLoad: function () {
     var that = this
     if (app.globalData.userInfo) {
@@ -143,53 +155,12 @@ textChange:function(e){
     })
   },
 
-  uploadImageToCloud : function() {
+
+
+  addAccuseToCloud : function() {
+
+    var _this = this
     
-    wx.showLoading({
-       title: '上传图片中',
-    })
-
-    const filePath = this.data.imageSrc
-    console.log(filePath)
-    // 上传图片
-    var num=Math.random()*100
-    const cloudPath = num + filePath.match(/\.[^.]+?$/)[0]
-    wx.cloud.uploadFile({
-      cloudPath,
-      filePath,
-      success: res => {
-        console.log('[上传文件] 成功：', res)
-
-        app.globalData.fileID = res.fileID
-        app.globalData.cloudPath = cloudPath
-        app.globalData.imagePath = filePath
-        console.log(filepath)
-        wx.showLoading({
-          title: '上传图片成功',
-       })
-        // this.setData({
-        //   pictureUrl:filepath
-        // })
-      },
-      fail: e => {
-        console.error('[上传文件] 失败：', e)
-        wx.showToast({
-          icon: 'none',
-          title: '上传失败',
-        })
-      },
-      complete: () => {
-        wx.hideLoading()
-      }
-    })
-  },
-  submit:function(){
-    //if (app.globalData.openid) {
-    //  this.setData({
-    //    openid: app.globalData.openid
-    //  })
-    //}
-
     const db = wx.cloud.database()
     console.log(app.globalData.fileID)
     db.collection('accuse').add({
@@ -200,21 +171,103 @@ textChange:function(e){
       },
       success: res => {
         // 在返回结果中会包含新创建的记录的 _id
-        wx.showToast({
-          title: '新增记录成功',
-        })
+        _this.clearContent()
+
         console.log('[数据库] [新增记录] 成功，记录 _id: ', res._id)
+        wx.showToast({
+          title: '提交举报成功'
+        })
       },
       fail: err => {
+        console.error('[数据库] [新增记录] 失败：', err)
         wx.showToast({
           icon: 'none',
-          title: '新增记录失败'
+          title: '提交举报失败'
         })
-        console.error('[数据库] [新增记录] 失败：', err)
+      },
+      complete: function(){
       }
     })
-    this.uploadImageToCloud()
+
   },
+
+
+  submit:function(){
+    //if (app.globalData.openid) {
+    //  this.setData({
+    //    openid: app.globalData.openid
+    //  })
+    //}
+
+    var _this = this
+
+    
+    // this.uploadImageToCloud()
+    
+    wx.showLoading({
+      title: '上传图片中',
+    })
+    const filePath = this.data.imageSrc
+    console.log(filePath)
+    // 上传图片
+    var num=Math.random()*100
+    const cloudPath = num + filePath.match(/\.[^.]+?$/)[0]
+
+    wx.cloud.uploadFile({
+      cloudPath,
+      filePath,
+
+      fail: e => {
+        console.error('[上传文件] 失败：', e)
+        wx.hideLoading()
+        wx.showToast({
+          icon: 'none',
+          title: '提交举报失败',
+        })
+      },
+      
+      complete: () => {
+      },
+
+      success: res => {
+        console.log('[上传文件] 成功：', filePath, cloudPath, res)
+
+        app.globalData.fileID = res.fileID
+        app.globalData.cloudPath = cloudPath
+        app.globalData.imagePath = filePath
+
+        wx.hideLoading()
+        wx.showToast({
+          title: '上传图片成功',
+        })
+
+        _this.addAccuseToCloud()
+
+      }
+    })
+  },
+
+
+    
+    // accusecause:[
+    //   {
+    //     id:1,
+    //     cause:"王"
+    //   },
+    //   {
+    //     id:2,
+    //     cause:"者"
+    //   },
+    //   {
+    //     id:3,
+    //     cause:"荣"
+    //   },
+    //   {
+    //     id:4,
+    //     cause:"耀"
+    //   },
+    // ],
+
 // onShow:function(){
 //   const db = wx.cloud.database()
 //     db.collection('seat').field({
